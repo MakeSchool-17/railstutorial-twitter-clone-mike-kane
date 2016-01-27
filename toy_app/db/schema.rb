fcdacfb1281d936b48eb6177749d7d1ea4bf2e91
Mike Kane <mike.kane@students.makeschool.com>
2016-01-26 18:16:23 -0800

    finish toy app, ch 2

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125233443) do
+ActiveRecord::Schema.define(version: 20160127012728) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
